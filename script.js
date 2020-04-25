1079c8f8b029a5b10ac39039b907e6c11a612e67
johnhanlon86 <57545003+johnhanlon86@users.noreply.github.com>
2020-04-25 22:29:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,9 +7,27 @@ $(document).ready(function() {
 
 // Side Navigation Bar (push to right).
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+// function openNav() {
+// document.getElementById("mySidenav").style.width = "250px";
+// document.getElementById("wrapper").style.marginLeft = "250px";
+// }
+
+// Side Navigation Bar (push to right).
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+// Added JavaScript function within Open Nav function so that it will not push screen/content for smaller screens.
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("wrapper").style.marginLeft = "250px";
+  function myFunction(x) {
+    if (x.matches) { // If media query matches
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("mySidenav").style.zIndex = "3";
+    } else {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("wrapper").style.marginLeft = "250px";
+    }
+  }
+  var x = window.matchMedia("(max-width: 1000px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
